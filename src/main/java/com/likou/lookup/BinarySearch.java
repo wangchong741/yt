2b231af284d467002a31c086620380de8e7ccf81
王冲 <853967228@qq.com>
2021-07-08 16:17:04 +0800

    二分查找算法

---
+++ src/main/java/com/likou/lookup/BinarySearch.java
@@ -0,0 +1,29 @@
+package com.likou.lookup;
+
+/**
+ * 二分查找
+ *
+ * @author 技惊四座
+ * @date 2021-07-08 15:21
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+        int binarySearch = binarySearch(new int[]{1, 2, 3, 4, 5, 6, 7}, 2);
+        System.out.println(binarySearch);
+    }
+
+    static int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
